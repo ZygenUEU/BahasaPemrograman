c81ab2bcfab1fe6106577ac483c5ab9ebed2cfc4
ZygenUEU <lamdapratama573@student.esaunggul.ac.id>
2022-10-17 11:22:06 +0700

    Bahasa Pemrograman

---
+++ Pertemuan_5/looping.py
@@ -0,0 +1,12 @@
+## looping with range
+for i in range(3):
+    print (i)
+
+for a in "pemograman":
+    if a == "p":
+        continue
+    if a == "g":
+        continue
+        #break
+    print (a)
+print ("End")
