7041d8867a5d09d27fa70b174d5455810ab5d138
ZygenUEU <lamdapratama573@student.esaunggul.ac.id>
2022-12-20 07:21:53 +0700

    Bahasa Pemrograman

---
+++ Pertemuan_12/Function.py
@@ -0,0 +1,11 @@
+def penjumlahan(angka1, angka2):
+    print(angka1, " + ", angka2, " = ", (angka1+angka2))
+
+penjumlahan(5, 6) #OUTPUT : 5 + 6 = 11
+
+# penjumlahan(5, 6) #ERROR : NameError: name 'penjumlahan' is not defined
+
+# def penjumlahan(angka1, angka2):
+#     print(angka1, " + ", angka2, " = ", (angka1+angka2))
+
+# penjumlahan(5, 6) #OUTPUT : 5 + 6 = 11
